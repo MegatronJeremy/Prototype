a1f8724e8c8e53eed3e52ebd604dac8c1251ea95
Vuk Djordjevic <vukd10@gmail.com>
2023-10-22 09:16:43 +0200

    Initial commit

---
+++ Prototype/Prototype.cpp
@@ -0,0 +1,12 @@
+﻿// Prototype.cpp : Defines the entry point for the application.
+//
+
+#include "Prototype.h"
+
+using namespace std;
+
+int main() {
+    cout << "Line one " << endl;
+    cout << "Hello CMake." << endl;
+    return 0;
+}
